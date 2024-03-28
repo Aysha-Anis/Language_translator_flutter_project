12746a2b3395de1153c95c7853e2a3cd3c2e46eb
Aysha Anis Roza <ayshaanisroza@gmail.com>
2024-03-28 16:01:24 +0600

    add files

---
+++ lib/language_translator.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
+
+class LanguageTranslationClass extends StatefulWidget {
+  const LanguageTranslationClass({super.key});
+
+  @override
+  State<LanguageTranslationClass> createState() => _LanguageTranslationClassState();
+}
+
+class _LanguageTranslationClassState extends State<LanguageTranslationClass> {
+
+  var Language = ['Hindi','English','Bangla'];
+  var originLanguage = "From";
+  var destinationLanguage = "To";
+  var output ="";
+  TextEditingController languageController =  TextEditingController();
+
+  void translate(String src,String dest , String input)async{
+    GoogleTranslator translator = new GoogleTranslator();
+    var translation = await translator.translate(input,from: src,to: dest);
+    setState(() {
+      output = translation.text.toString();
+    });
+
+    if(src=='--'||dest=='--')
+    {
+      setState(() {
+        output = "fail to translate";
+      });
+    }
+
+
+  }
+
+
+  String getLanguageCode(String Language){
+    if(Language=="English"){
+      return 'en';
+    }
+    else if (Language=="Hindi"){
+      return "hi";
+    }
+    else if(Language=="Bangla"){
+      return "bn";
+    }
+    return "--";
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff10223d),
+      appBar: AppBar(title: Text("Language Translator"),centerTitle: true,
+      backgroundColor: Color(0xff10223d),
+      elevation: 0,
+      
+      ),
+      body: Center(
+        child: SingleChildScrollView(child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                DropdownButton(focusColor: Colors.white,
+                  iconDisabledColor: Colors.white,
+                  iconEnabledColor: Colors.white,
+                  hint: Text(
+                    originLanguage,style: TextStyle(color: Colors.white),
+                  ),
+                  dropdownColor: Colors.white,
+                  icon:Icon(Icons.keyboard_arrow_down), 
+                  items: Language.map((String dropDownStringItem){
+                    return DropdownMenuItem(child: Text(dropDownStringItem),value: dropDownStringItem,);
+                  }).toList(),
+                  onChanged: (String? value){
+                    setState(() {
+                      originLanguage = value!;
+                    });
+
+
+                  },
+                ),
+
+                SizedBox(width: 40,),
+                Icon(Icons.arrow_right_alt_outlined,color: Colors.white,size: 40,),
+                SizedBox(width: 40,),
+
+
+                DropdownButton(focusColor: Colors.white,
+                  iconDisabledColor: Colors.white,
+                  iconEnabledColor: Colors.white,
+                  hint: Text(
+                    destinationLanguage,style: TextStyle(color: Colors.white),
+                  ),
+                  dropdownColor: Colors.white,
+                  icon:Icon(Icons.keyboard_arrow_down), 
+                  items: Language.map((String dropDownStringItem){
+                    return DropdownMenuItem(child: Text(dropDownStringItem),value: dropDownStringItem,);
+                  }).toList(),
+                  onChanged: (String? value){
+                    setState(() {
+                      destinationLanguage = value!;
+                    });
+
+
+                  },
+                ),
+
+
+              ],
+            ),
+            SizedBox(height: 40,),
+            Padding(padding: EdgeInsets.all(8),
+            
+            child: TextFormField(
+              cursorColor: Colors.white,
+              autofocus: false,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelText: 'Please enter your text :',
+                labelStyle: TextStyle(fontSize: 15,color: Colors.white),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 1,
+                  )
+                ),
+                enabledBorder: OutlineInputBorder(
+                   borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 1
+                    
+                  )
+                ),
+                errorStyle: TextStyle(color: Colors.red,fontSize: 15),
+              ),
+              controller: languageController,
+              validator: (value){
+                if(value==null || value.isEmpty)
+                {
+                  return 'Please enter text to translate :';
+                }
+                return null;
+              },
+            ),),
+           Padding(padding: EdgeInsets.all(8),
+           child: ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Color(0xff10223d)),
+            onPressed: (){
+              translate(getLanguageCode(originLanguage), getLanguageCode(destinationLanguage), languageController.text.toString());
+            }, child: Text("Translate"))
+           ),
+           SizedBox(height: 20,),
+           Text(
+            "\n$output",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+           )
+          
+          ],
+          ),),),
+    );
+  }
+}
